0857eed2e916839f143d15c9670a11bc58e1c0d0
Jirigesi <50706568+Jirigesi@users.noreply.github.com>
2021-04-09 14:15:00 -0700

    Add files via upload

---
+++ runDC.py
@@ -0,0 +1,66 @@
+# move and rename splitted files into specific folder 
+
+# train new model multiple times and log results especially precision, recall, F1, AUC 
+
+# run each model 10 times and save results into an csv file 
+from JITLine_replication_package.JITLine.JITLine_run import create_path_if_not_exist, run_experiment, eval_result
+import os 
+import subprocess
+import csv
+
+
+
+# os.chdir("/home/fjiriges/model_execution")
+# characters= ["Filecount", "Editcount", "MultilineCommentscount"]
+characters= ["Filecount"]
+levels = [ "hard", "easy"]
+
+for character in characters:
+    for level in levels:
+        os.chdir("/home/fjiriges/model_execution")
+        # Each time must copy correspoding training and testing data
+        test_file_to_move = "splittedData/" + level + "_" + character + "_openstack_test.pkl"
+        train_file_to_move = "splittedData/" + level + "_" + character + "_openstack_train.pkl"
+        # splittedData / easy_Filecount_openstack_train.pkl
+
+        subprocess.call(["mv", test_file_to_move, "JITLine_replication_package/JITLine/data/openstack_test.pkl"])
+        print("moved testing files")
+
+        os.chdir("/home/fjiriges/model_execution")
+        subprocess.call(["mv", train_file_to_move, "JITLine_replication_package/JITLine/data/openstack_train.pkl"])
+        print("moved training files")
+
+        os.chdir("/home/fjiriges/model_execution/JITLine_replication_package/JITLine")
+        for i in range(10):
+            ######
+            projects = ['openstack','qt']
+            sampling_methods = 'DE_SMOTE_min_df_3'
+            remove_python_common_tokens = True
+            # os.chdir("JITLine_replication_package/JITLine")
+            #
+            # create_path_if_not_exist('./data/')
+            # create_path_if_not_exist('./final_model/')
+
+            # Run experiment
+            # qt_k_of_smote, qt_best_AUC_of_obj_func = run_experiment('qt')
+            qt_k_of_smote, qt_best_AUC_of_obj_func = run_experiment('openstack')
+
+            # print('The best k_neighbors of Qt:', qt_k_of_smote)
+
+            RF_data_dir = './data/'
+
+            # eval_result('qt')
+            results = eval_result('openstack')
+            # store results
+
+            with open('DCresults.csv', mode='a+') as csv_file:
+                # fieldnames = ["prec", "rec", "f1", "auc"]
+                csvwriter = csv.writer(csv_file)
+                # writer.writeheader()
+                csvwriter.writerow([level, character, i, results[0], results[1], results[2], results[3]])
+
+            # move result to a collection folder 
+            result_new_name = str(i) + "_" + level + "_" + character
+
+            subprocess.call(["mv", "/home/fjiriges/model_execution/JITLine_replication_package/JITLine/data/openstack_RF_DE_SMOTE_min_df_3_prediction_result.csv",
+                             "/home/fjiriges/model_execution/JITLine_replication_package/result_collctor/" + result_new_name])
---
+++ split_data.py
@@ -0,0 +1,71 @@
+import pickle 
+import pandas as pd
+
+def splitData(commit_infor_file:str, original_file:str, threshold, characteristic):
+
+    df = pd.read_csv(commit_infor_file)
+
+    data = pickle.load(open(original_file, 'rb'))
+    ids, labels, msgs, codes = data
+    print("Total number of data:", len(ids))
+    
+    easy_ids = []
+    easy_labels = []
+    easy_msgs = []
+    easy_codes = []
+
+    hard_ids = []
+    hard_labels = []
+    hard_msgs = []
+    hard_codes = []
+
+    for index, id_ in enumerate(ids):
+        value = df.loc[df['Commit_Hash'] == id_, characteristic].iloc[0]
+        if value <= threshold:
+            easy_ids.append(id_)
+            easy_labels.append(labels[index])
+            easy_msgs.append(msgs[index])
+            easy_codes.append(codes[index])
+
+        else:
+            hard_ids.append(id_)
+            hard_labels.append(labels[index])
+            hard_msgs.append(msgs[index])
+            hard_codes.append(codes[index])
+
+    easy_data = (easy_ids, easy_labels, easy_msgs, easy_codes)
+    print("Splited the easy part: ", len(easy_ids))
+    
+    file1name = 'splittedData/easy_' +characteristic + "_" + original_file.split('/')[-1] 
+    
+    with open(file1name, 'wb') as handle:
+        pickle.dump(easy_data, handle)
+        
+    hard_data = (hard_ids, hard_labels, hard_msgs, hard_codes)
+    print("Splited the hard part: ", len(hard_ids))
+    file2name = 'splittedData/hard_' + characteristic + "_" + original_file.split('/')[-1] 
+    with open(file2name, 'wb') as handle:
+        pickle.dump(hard_data, handle)
+
+
+
+if __name__ == "__main__":
+
+    # commit_infor_file = "/Users/fjirigesi/Desktop/Jiri_openstack_train_infor.csv"
+    # original_file = '/Users/fjirigesi/Downloads/data+model/data/jit/openstack_train.pkl'
+
+    commit_infor_file = "/Users/fjirigesi/Documents/defect_prediction_unfaieness-main/DeepJIT/OSresults/OS_result.csv"
+    original_file = "/Users/fjirigesi/Downloads/data+model/data/jit/openstack_test.pkl"
+
+    threshold = 6.04
+    characteristic = "Filecount"
+
+    OS_threshold_dict = {
+        "Filecount": 6.04,
+        "Editcount": 143.3,
+        "MultilineCommentscount": 11.6,
+        "Inwards_sum": 15.51,
+        "Outwards_sum": 48.04
+    }
+
+    splitData(commit_infor_file, original_file, threshold, characteristic)
